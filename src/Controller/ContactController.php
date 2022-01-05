b7c17fa77634479f09fb1a56b2709064f0b8b403
HUGUET Zachary <huguzac@ndlp.fr>
2022-01-05 12:41:21 +0100

    first

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+     /**
+     * @Route("/envoi_mail", name="envoi_mail")
+     */
+    public function envoiMail(): Response
+    {
+        
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
