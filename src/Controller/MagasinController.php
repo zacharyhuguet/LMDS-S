bb38c4b08f4e63612cedca9a1df0ca743bcd1588
HUGUET Zachary <huguzac@ndlp.fr>
2022-01-06 10:32:16 +0100

    loginOk

---
+++ src/Controller/MagasinController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MagasinController extends AbstractController
+{
+    /**
+     * @Route("/magasin", name="magasin")
+     */
+    public function index(): Response
+    {
+        return $this->render('magasin/index.html.twig', [
+            'controller_name' => 'MagasinController',
+        ]);
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+
+        public function load(ObjectManager $manager): void
+    {
+        $superadmin = new User();                                                     ///création de l'utilisateur superAdmin
+        $superadmin->setUsername('superadmin');
+        $superadmin->setRoles(array('ROLE_SUPER_ADMIN'));
+        $password = $this->passwordEncoder->encodePassword($superadmin, 'superadmin');
+        $superadmin->setPassword($password);
+        $manager->persist($superadmin);
+
+        $admin = new User();                                                           ///création de l'utilisateur Admin
+        $admin->setUsername('admin');
+        $admin->setRoles(array('ROLE_ADMIN'));
+        $password = $this->passwordEncoder->encodePassword($admin, 'admin');
+        $admin->setPassword($password);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
