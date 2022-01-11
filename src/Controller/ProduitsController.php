73663c1b663dea7310cd904dd97437d206664eb9
HUGUET Zachary <huguzac@ndlp.fr>
2022-01-11 15:23:54 +0100

    articleOk

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,20 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProduitsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProduitsController extends AbstractController
 {
     /**
      * @Route("/produits", name="produits")
      */
-    public function index(): Response
+    public function index(ProduitsRepository $repository): Response
     {
+        $produits = $repository->findAll();
         return $this->render('produits/index.html.twig', [
             'controller_name' => 'ProduitsController',
+            'produits' => $produits
         ]);
     }
 }
 
+// public function index(InformationAccueilRepository $repository): Response
+// {
+//     $infos = $repository->findAll();
+//     return $this->render('home/index.html.twig', [
+//         'controller_name' => 'HomeController',
+//         'infos' => $infos
+//     ]);
+// }
