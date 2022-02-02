be0022e17cd1ab66a0682351c62c9835f834df49
HUGUET Zachary <huguzac@ndlp.fr>
2022-02-02 13:50:42 +0100

    fixbug2

--- src/Controller/DevisAdminController.php
+++ src/Controller/DevisAdminController.php
@@ -351,13 +351,13 @@ class DevisAdminController extends AbstractController
         $status = $devi->getStatus();
         
         if ($status == "Nouveau-Devis"){
-            $devi->setStatus("En cours");
+            $devi->setStatus("En-cours");
         }
-        if ($status == "En cours"){
+        if ($status == "En-cours"){
             $devi->setStatus("Fini");
         }
         if ($status == "Fini"){
-            $devi->setStatus("Nouveau devis");
+            $devi->setStatus("Nouveau-Devis");
         }
         
         $entityManager->flush();
